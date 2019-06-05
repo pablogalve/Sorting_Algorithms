2213454357601158499b5e68caa80a0b331391af
Pablo <eduinvestor100@gmail.com>
2019-06-05 10:56:10 +0200

    merge sort working

--- SortingAlgorithms/ex1.h
+++ SortingAlgorithms/ex1.h
@@ -35,32 +35,53 @@ void bubblesort(uint32 * array, int size)
 
 // Mergesort functions /////////////////////////////////////////////////
 
-void merge(uint32 *array, int begin, int mid, int end)
+void merge(uint32 *arr, int l, int m, int r)
 {
-	// Create the two subarrays
-	// TODO
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
 
-	// Copy array parts into the subarrays
-	// TODO
+	int *L, *R; // sub-arrays a fusionar
+	L = new int[n1];
+	R = new int[n2];
 
-	// Initialize indices for tmp1, tmp2, and array, respectively
-	int i = 0;
-	int j = 0;
-	int k = begin;
+	for (i = 0; i < n1; i++) L[i] = arr[l + i];
+	for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
 
-	// Merge tmp1 and tmp2 into array
-	// TODO
+	// We merge subarrays at: arr[l..r]
+	i = 0; // initial index from first subarray 
+	j = 0; // final index from the second subarray 
+	k = l; // initial index of merged subarray 
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j])  arr[k] = L[i++];
+		else		       arr[k] = R[j++];
+		k++;
+	}
 
-	// Delete tmp arrays
-	// TODO
-	
+	while (i < n1) { 
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) { 
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+	delete[] L;
+	delete[] R;
 }
 
-void mergesort(uint32 *array, int begin, int end)
+void mergesort(uint32 *arr, int l, int r)
 {
-	
+	if (l < r) {
+		int m = l + (r - l) / 2;     // m: mid index
 
-
+		mergesort(arr, l, m);        // we sort array's left fragment  
+		mergesort(arr, m + 1, r);    // we sort array's right fragment  
+		merge(arr, l, m, r);         // we merge both sub-arrays
+	}
 }
 
 
